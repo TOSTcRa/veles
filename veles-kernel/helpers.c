78bc57ec1ba0caf56091b219da9ee7e91d1edc74
TOSTcRa <antostcr@gmail.com>
2026-03-21 20:57:00 +0100

    sections are now read via vm_mmap and are loaded into the memory, new helper func

--- veles-kernel/helpers.c
+++ veles-kernel/helpers.c
@@ -1,5 +1,26 @@
 #include <linux/binfmts.h>
-
+#include <linux/fs.h>
+#include <linux/uaccess.h>
 char *get_buf(struct linux_binprm *bprm) { return bprm->buf; }
 
-struct file *get_file(struct linux_binprm *bprm) { return bprm->file; }
+struct file *veles_get_file(struct linux_binprm *bprm) { return bprm->file; }
+
+ssize_t veles_get_to_user(struct file *file, unsigned long addr, loff_t pos,
+                          size_t len) {
+  while (len > 0) {
+    size_t chunk = len > 512 ? 512 : len;
+    char buf[512];
+    ssize_t ret = kernel_read(file, buf, chunk, &pos);
+    if (ret <= 0) {
+      return ret;
+    }
+    ssize_t res = copy_to_user((void __user *)addr, buf, ret);
+    if (res != 0) {
+      return -14;
+    }
+    addr += ret;
+    len -= ret;
+  }
+
+  return 0;
+}
--- veles-kernel/module.rs
+++ veles-kernel/module.rs
@@ -31,9 +31,22 @@ extern "C" {
 
     fn get_buf(bprm: *mut linux_binprm) -> *const u8;
 
-    fn get_file(bprm: *mut linux_binprm) -> *mut c_void;
+    fn veles_get_file(bprm: *mut linux_binprm) -> *mut c_void;
 
     fn kernel_read(file: *mut c_void, buf: *mut u8, count: usize, pos: *mut i64) -> isize;
+
+    fn vm_mmap(
+        file: *mut c_void,
+        addr: c_ulong,
+        size: c_ulong,
+        prot: c_ulong,
+        flags: c_ulong,
+        offset: c_ulong,
+    ) -> c_ulong;
+
+    fn begin_new_exec(bprm: *mut linux_binprm) -> c_int;
+
+    fn veles_get_to_user(file: *mut c_void, addr: c_ulong, offset: i64, size: usize) -> isize;
 }
 
 unsafe extern "C" fn load_pe_binary(bprm: *mut linux_binprm) -> c_int {
@@ -49,38 +62,65 @@ unsafe extern "C" fn load_pe_binary(bprm: *mut linux_binprm) -> c_int {
                 if (buf.add(offset as usize) as *const u32).read_unaligned() == 0x00004550 {
                     pr_info!("got the PE header\n");
 
-                    if (buf.add(offset as usize + 4) as *const u16).read_unaligned() == 0x8664 {
-                        pr_info!("AMD64 arch\n");
-                    } else {
-                        pr_info!("not an AMD64 arch\n");
-                    }
-
-                    let sections = (buf.add(offset as usize + 6) as *const u16).read_unaligned();
-                    pr_info!("{} sections\n", sections);
-
-                    let entry_point =
-                        (buf.add(offset as usize + 40) as *const u32).read_unaligned();
-                    pr_info!("{:#x}, is the entry point\n", entry_point);
-
-                    let file = get_file(bprm);
+                    let file = veles_get_file(bprm);
                     let mut pe_buf = [0u8; 1024];
                     let mut pos: i64 = 0;
                     let pointer = &mut pe_buf[0] as *mut u8;
                     kernel_read(file, pointer, 1024, &mut pos);
 
+                    if (pointer.add(offset as usize + 4) as *const u16).read_unaligned() == 0x8664 {
+                        pr_info!("AMD64 arch\n");
+                    } else {
+                        pr_info!("not an AMD64 arch\n");
+                    }
+
+                    let sections =
+                        (pointer.add(offset as usize + 6) as *const u16).read_unaligned();
+                    pr_info!("{} sections\n", sections);
+
+                    let entry_point =
+                        (pointer.add(offset as usize + 40) as *const u32).read_unaligned();
+                    pr_info!("{:#x}, is the entry point\n", entry_point);
+
                     let size_of_header =
                         (pointer.add(offset as usize + 20) as *const u16).read_unaligned();
 
+                    let image_base =
+                        (pointer.add(offset as usize + 48) as *const u64).read_unaligned();
+
+                    pr_info!("{:#x} is the image_base\n", image_base);
+
+                    let ret = begin_new_exec(bprm);
+                    if ret != 0 {
+                        return ret;
+                    }
                     let start = offset as usize + 4 + 20 + size_of_header as usize;
                     for i in 0..sections {
                         let sec = start + i as usize * 40;
 
+                        let addr = (pointer.add(sec + 12) as *const u32).read_unaligned();
+                        let size = (pointer.add(sec + 16) as *const u32).read_unaligned();
+                        let offest_in_file = (pointer.add(sec + 20) as *const u32).read_unaligned();
                         pr_info!(
                             "addres in memory: {}, size: {}, offset in file: {}\n",
-                            (pointer.add(sec + 12) as *const u32).read_unaligned(),
-                            (pointer.add(sec + 16) as *const u32).read_unaligned(),
-                            (pointer.add(sec + 20) as *const u32).read_unaligned(),
+                            addr,
+                            size,
+                            offest_in_file,
                         );
+
+                        let mmap_addr = (image_base + addr as u64) as usize;
+
+                        let result =
+                            vm_mmap(core::ptr::null_mut(), mmap_addr, size as usize, 7, 0x32, 0);
+
+                        if (result as isize) < 0 && (result as isize) > -4096 {
+                            return -1;
+                        }
+
+                        pr_info!("{:#x}, is the result of vm_mmap", result);
+
+                        let file_pos = offest_in_file as i64;
+                        veles_get_to_user(file, mmap_addr as c_ulong, file_pos, size as usize);
                     }
                 }
             }
